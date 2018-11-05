fcc840122068a71601fc1986651ce4fae4d721b4
Tesfahun Berhanu <tberhanu@berkeley.edu>
2018-11-05 08:42:23 -0800

    try pushing again with settings/base.py file

--- AAAinfo/github.py
+++ AAAinfo/github.py
@@ -4,10 +4,11 @@
     schoolAdmin$ git commit -m "schoolAdmin/settings/sensitive_file.py"
     schoolAdmin$ git push (-f)* origin master
 2. Deleting already pushed file both from the file and from the github
-    schoolAdmin$ git rm sentisive_file_location_path.property
+    schoolAdmin$ git rm sentisive_file_location_path.py
     schoolAdmin$ git commit -m "schoolAdmin/settings/sensitive_file.py"
     schoolAdmin$ git push (-f)* origin master
-
+3. Add the full path of the sensitive_file you don't want to push to version control, github like:
+    .gitignore --> schoolAdmin/settings/secured_info.py
 
 
 
