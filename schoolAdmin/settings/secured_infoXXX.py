6e842baa3e181bb579c35ef4e1342a74d6cf9d18
Tesfahun Berhanu <tberhanu@berkeley.edu>
2018-11-09 15:45:48 -0800

    deleting some sensitive files from github push and including in gitignore file

--- schoolAdmin/settings/secured_infoXXX.py
+++
@@ -1,9 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tesfahunt@gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'Etete@2014'
-
-
-PASSWORD='tested'
-SECRET_KEY='8v+d46cn4$s(o@u&cjm3fkdl50((m9e3r+3h92z0z@#nxn9)8n'
