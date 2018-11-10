bbfc7fbf0df05e12c3f9207c98ea16c6d378188f
Tesfahun Berhanu <tberhanu@berkeley.edu>
2018-11-09 18:00:25 -0800

    pythonanywhere

--- schoolAdmin/wsgi.py
+++ schoolAdmin/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolAdmin.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolAdmin.settings")
 
 application = get_wsgi_application()
